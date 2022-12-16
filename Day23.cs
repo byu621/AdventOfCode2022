1196ea132dd313d0fcee064aeea6bde5957a1932
byu621 <55732851+byu621@users.noreply.github.com>
2022-12-16 17:18:12 +1300

    set up all 25 days because I have no internet and im bored

---
+++ Day23.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    class Day23
+    {
+        public void Star1(string input)
+        {
+            int output = 0;
+            string[] lines = File.ReadAllLines(input);
+            foreach (string line in lines)
+            {
+
+            }
+
+            Console.WriteLine(output);
+        }
+
+        public void Star2(string input)
+        {
+            int output = 0;
+            string[] lines = File.ReadAllLines(input);
+            foreach (string line in lines)
+            {
+
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
