8faa24b838d5266e1d68dbb0f73f7cacc92e432b
Benjamin-Yu <ben.yu2@datacom.co.nz>
2022-12-20 18:50:40 +1300

    day 20

--- Day20.cs
+++ Day20.cs
@@ -10,28 +10,162 @@ namespace AdventOfCode2022
 {
     class Day20
     {
+        private List<string> circle = new List<string>();
+        private HashSet<string> set = new HashSet<string>();
+        private int id;
         public void Star1(string input)
         {
             int output = 0;
             string[] lines = File.ReadAllLines(input);
             foreach (string line in lines)
             {
+                string actualValue = line;
+                if (set.Contains(actualValue))
+                {
+                    actualValue += $"%{id++}";
+                }
 
+                circle.Add(actualValue);
+                set.Add(actualValue);
             }
 
-            Console.WriteLine(output);
+            List<string> originalOrder = new List<string>(circle);
+
+            for (int i = 0; i < originalOrder.Count; i++)
+            {
+                string numberToMove = originalOrder[i];
+                int index = circle.IndexOf(numberToMove);
+
+                int value = int.Parse(circle[index].Split('%')[0]);
+                if (value == 0)
+                {
+                    continue;
+                }
+
+                for (int j = 0; j < Math.Abs(value); j++)
+                {
+                    if (value < 0)
+                    {
+                        index = swapBack(index);
+                    } else
+                    {
+                        index = swapForward(index);
+                    }
+                }
+            }
+
+            int outputA = (1000 + circle.IndexOf("0")) % circle.Count;
+            int outputB = (2000 + circle.IndexOf("0")) % circle.Count;
+            int outputC = (3000 + circle.IndexOf("0")) % circle.Count;
+            int a = int.Parse(circle[outputA].Split('%')[0]);
+            int b = int.Parse(circle[outputB].Split('%')[0]);
+            int c = int.Parse(circle[outputC].Split('%')[0]);
+
+            Console.WriteLine(a + b + c );
+        }
+
+        private int swapForward(int index)
+        {
+            if (index == circle.Count - 1)
+            {
+                swap(index, 0);
+                return 0;
+            }
+            else
+            {
+                swap(index, index + 1);
+                return index + 1;
+            }
+        }
+
+        private int swapBack(int index)
+        {
+            if (index == 0)
+            {
+                swap(0, circle.Count-1);
+                return circle.Count - 1;
+            }
+            else
+            {
+                swap(index, index - 1);
+                return index - 1;
+            }
+        }
+
+        private void swap(int indexA, int indexB)
+        {
+            string temp = circle[indexA];
+            circle[indexA] = circle[indexB];
+            circle[indexB] = temp;
         }
 
         public void Star2(string input)
         {
             int output = 0;
+            long decryptionKey = 811_589_153;
             string[] lines = File.ReadAllLines(input);
             foreach (string line in lines)
             {
+                long temp = long.Parse(line) * decryptionKey;
+                string actualValue = temp.ToString();
+                if (set.Contains(actualValue))
+                {
+                    actualValue += $"%{id++}";
+                }
 
+                circle.Add(actualValue);
+                set.Add(actualValue);
             }
 
-            Console.WriteLine(output);
+            List<string> originalOrder = new List<string>(circle);
+
+            for (int iter = 0; iter < 10; iter++)
+            {
+                for (int i = 0; i < originalOrder.Count; i++)
+                {
+                    string numberToMove = originalOrder[i];
+                    int index = circle.IndexOf(numberToMove);
+
+                    long value = long.Parse(circle[index].Split('%')[0]);
+                    if (value == 0)
+                    {
+                        continue;
+                    }
+
+                    //while (value < circle.Count)
+                    //{
+                    //    value += circle.Count;
+                    //}
+
+                    //while (value > circle.Count)
+                    //{
+                    //    value -= circle.Count;
+                    //}
+
+                    value %= (circle.Count-1);
+
+                    for (int j = 0; j < Math.Abs(value); j++)
+                    {
+                        if (value < 0)
+                        {
+                            index = swapBack(index);
+                        }
+                        else
+                        {
+                            index = swapForward(index);
+                        }
+                    }
+                }
+            }
+
+            int outputA = (1000 + circle.IndexOf("0")) % circle.Count;
+            int outputB = (2000 + circle.IndexOf("0")) % circle.Count;
+            int outputC = (3000 + circle.IndexOf("0")) % circle.Count;
+            long a = long.Parse(circle[outputA].Split('%')[0]);
+            long b = long.Parse(circle[outputB].Split('%')[0]);
+            long c = long.Parse(circle[outputC].Split('%')[0]);
+
+            Console.WriteLine(a + b + c);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,23 +12,9 @@ namespace AdventOfCode2022
 
         static void Main(string[] args)
         {
-            //new Day16Star2().Star2($"{Environment.CurrentDirectory}/Input/day16sample.txt");
-            //new Day17().Star1($"{Environment.CurrentDirectory}/Input/day17sample.txt");
-            //Dictionary<int, int> dict = new Dictionary<int, int>();
-            //for (int i = 1; i < 100; i++)
-            //{
-            //    Console.WriteLine(i);
-            //    dict.Add(i, new Day17().Star1($"{Environment.CurrentDirectory}/Input/day17.txt", i));
-            //    Console.WriteLine(dict[i]);
-            //    if (i % 2 == 0 && dict[i] % dict[i / 2] == 0)
-            //    {
-            //        Console.WriteLine("got em");
-            //    }
-            //}
-            //new Day17().Star1($"{Environment.CurrentDirectory}/Input/day17sample.txt", 1);
-            //new Day18().Star1($"{Environment.CurrentDirectory}/Input/day18sample.txt");
-            //new Day19().Star1($"{Environment.CurrentDirectory}/Input/day19sample.txt");
-            new Day19().Star1($"{Environment.CurrentDirectory}/Input/day19.txt");
+            //new Day20().Star1($"{Environment.CurrentDirectory}/Input/day20sample.txt");
+            new Day20().Star2($"{Environment.CurrentDirectory}/Input/day20sample.txt");
+            new Day20().Star2($"{Environment.CurrentDirectory}/Input/day20.txt");
             Console.WriteLine("finished");
             Console.ReadKey();
         }
