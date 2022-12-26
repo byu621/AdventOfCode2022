9c2ed0192f3108a2ab5168bb4dd53ecc15ff2fd7
Benjamin Yu <byu621@aucklanduni.ac.nz>
2022-12-26 14:04:59 +1300

    day 25 star 1

--- Day25.cs
+++ Day25.cs
@@ -13,13 +13,66 @@ namespace AdventOfCode2022
         public void Star1(string input)
         {
             int output = 0;
+            long decimalValue = 0;
             string[] lines = File.ReadAllLines(input);
             foreach (string line in lines)
             {
-
+                decimalValue += toDecimal(line);
             }
 
-            Console.WriteLine(output);
+            Console.WriteLine(decimalValue);
+
+            var snafu = new StringBuilder();
+            while (decimalValue > 0)
+            {
+                long a = decimalValue % 5;
+                if (a == 3)
+                {
+                    snafu.Append('=');
+                } else if (a == 4)
+                {
+                    snafu.Append('-');
+                } else
+                {
+                    snafu.Append(a);
+                }
+
+                decimalValue -= charToInt(snafu[snafu.Length - 1]);
+                decimalValue /= 5;
+            }
+
+            Console.WriteLine(new string(snafu.ToString().Reverse().ToArray()));
+            Console.WriteLine(toDecimal(new string(snafu.ToString().Reverse().ToArray())));
+        }
+
+        private long toDecimal(string line)
+        {
+            long decimalValue = 0;
+            long multiplier = 1;
+            for (int i = line.Length - 1; i >= 0; i--)
+            {
+                char c = line[i];
+                int value = charToInt(c);
+                decimalValue += value * multiplier;
+                multiplier *= 5;
+            }
+
+            return decimalValue;
+        }
+
+        private int charToInt(char c)
+        {
+            if (c == '-')
+            {
+                return -1;
+            }
+
+            if (c == '=')
+            {
+                return -2;
+            }
+
+            return c - '0';
         }
 
         public void Star2(string input)
--- Program.cs
+++ Program.cs
@@ -14,8 +14,8 @@ namespace AdventOfCode2022
         {
             //new Day20().Star1($"{Environment.CurrentDirectory}/Input/day20sample.txt");
             //new Day21().Star2($"{Environment.CurrentDirectory}/Input/day21sample.txt");
-            new Day24().Star1($"{Environment.CurrentDirectory}/Input/day24sample.txt");
-            new Day24().Star1($"{Environment.CurrentDirectory}/Input/day24.txt");
+            new Day22Star2().Star1($"{Environment.CurrentDirectory}/Input/day22sample.txt");
+            new Day22Star2().Star1($"{Environment.CurrentDirectory}/Input/day22.txt");
             Console.WriteLine("finished");
             Console.ReadKey();
         }
